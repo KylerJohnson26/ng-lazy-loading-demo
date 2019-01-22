5e1c23a61d4775cd594267fe7432da26d4d3074e
KylerJohnson26 <kylerljohnson@icloud.com>
2019-01-21 20:41:24 -0500

    adding product module, product component, and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', loadChildren: './product/product.module#ProductModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'path' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [ProductComponent],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
