add88d059aa54ff599d6c4497f6616c3bdba76df
KylerJohnson26 <kylerljohnson@icloud.com>
2019-02-09 09:39:32 -0500

    moving modular structure and lazy loading to a feature branch to show app working without them in master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './core/home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
-  { path: 'product', loadChildren: './product/product.module#ProductModule' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'product', component: ProductComponent },
   { path: '', component: HomeComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NavComponent,
+    ProductComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
