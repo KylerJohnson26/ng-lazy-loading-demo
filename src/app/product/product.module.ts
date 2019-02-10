f1e0829739ce7aafca63d502559d592a48e83ef9
KylerJohnson26 <kylerljohnson@icloud.com>
2019-02-09 19:56:19 -0500

    removing unnecessary profile and product module router configurations

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product/product.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ProductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule,
   ],
   exports: [
     ProductComponent
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
-import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [ProfileComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
   ],
   exports: [
     ProfileComponent
